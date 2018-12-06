ef3e4b8bc580a490fd64d08863870daf726a91a0
BuildTools <shanebolenback@me.com>
2018-12-06 15:24:48 -0800

    Small Fixes

--- src/tk/shanebee/skriptpaper/elements/expressions/ExprTurtleHome.java
+++ src/tk/shanebee/skriptpaper/elements/expressions/ExprTurtleHome.java
@@ -1,13 +1,9 @@
 package tk.shanebee.skriptpaper.elements.expressions;
 
-import ch.njol.skript.ScriptLoader;
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.*;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser;
-import ch.njol.util.Kleenean;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Turtle;
@@ -34,7 +30,6 @@ public class ExprTurtleHome extends SimplePropertyExpression<LivingEntity, Locat
             return ((Turtle) t).getHome();
         } else {
             return null;
-
         }
     }
 
@@ -46,7 +41,6 @@ public class ExprTurtleHome extends SimplePropertyExpression<LivingEntity, Locat
 
     @Override
     public void change(Event e, Object[] delta, ChangeMode mode) {
-
         if (delta == null) {
             for (final LivingEntity t : getExpr().getArray(e)) {
                 if (!(t instanceof Turtle)) return;
